5c504782b128db65d5ed54d973e6ab6c54755d20
Peter Garside-Quin <petegq@gmail.com>
2020-10-21 18:27:38 +0100

    Initial commit

---
+++ .prettierrc.js
@@ -0,0 +1,12 @@
+module.exports = {
+	semi: true,
+	singleQuote: false,
+	jsxSingleQuote: true,
+	useTabs: true,
+	tabWidth: 4,
+	bracketSpacing: true,
+	jsxBracketSameLine: true,
+	arrowParens: "avoid",
+	trailingComma: "none",
+	endOfLine: "lf"
+};
